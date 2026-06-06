15b45b6b9527faa06936623dacf0cbf47cfcfdc3
Pro-Wings <81178769+Pro-Wings@users.noreply.github.com>
2026-06-06 10:40:57 +0530

    dev2 changes

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,11 +2,12 @@ public class A {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World, welcome to git branching!!!");
+        System.out.println("Hello Java Developers..., welcome to git branching!!!");
 
         //print 1 to 100
-        for(int i = 1; i <= 50; i++) {
-            System.out.println("i = "+i);
+        for(int i = 1; i <= 150; i++) {
+            System.out.println("i = "+i  );
+            System.out.println("i = "+i+1  );
         }
 
     }
