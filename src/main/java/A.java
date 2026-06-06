0036a926d7ff2381f393ce0db285027169d5f0b0
Pro-Wings <81178769+Pro-Wings@users.noreply.github.com>
2026-06-06 10:35:00 +0530

    Update A.java

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,11 +2,11 @@ public class A {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, welcome to git branching!!!");
 
         //print 1 to 100
-        for(int i = 1; i <= 100; i++) {
-            System.out.println(i);
+        for(int i = 1; i <= 50; i++) {
+            System.out.println("i = "+i);
         }
 
     }
