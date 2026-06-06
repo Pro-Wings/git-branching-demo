c3d759c6eca948f395aff8c535237e304d6da21b
JD <jayprowings@gmail.com>
2026-06-06 10:42:15 +0530

    second draft from dev1

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,11 +2,11 @@ public class A {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World, welcome to git branching!!!");
+        System.out.println("git branching demo!!!");
 
         //print 1 to 100
-        for(int i = 1; i <= 50; i++) {
-            System.out.println("i = "+i);
+        for(int i = 1; i <= 20; i++) {
+            System.out.println("i = ");
         }
 
     }
