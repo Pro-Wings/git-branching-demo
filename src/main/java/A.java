e745dd205eec0e54ed6d8ebc6ffe68f8c8f94312
JD <jayprowings@gmail.com>
2026-06-06 10:36:45 +0530

    second draft

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,11 +2,12 @@ public class A {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
 
         //print 1 to 100
         for(int i = 1; i <= 100; i++) {
             System.out.println(i);
+            System.out.println(i+1);
         }
 
     }
